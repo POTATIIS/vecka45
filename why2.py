027965d5b9ef1d698f968dec31ff627b35526e19
POTATIIS <alexander.martinsson@elev.ga.ntig.se>
2020-11-02 14:06:12 +0100

    why2

--- why2.py
+++ why2.py
@@ -1,14 +1,12 @@
 import random
 randomnumber = random.randint(0,10)
 guess = 0
-attempt = 1
+attempt = 0
 while guess != randomnumber:
+    attempt += 1
     guess = float(input('guess a number between 0 to 10: '))
     if guess < randomnumber:
         print(f"{guess} is to low")
-        attempt += 1
     elif guess > randomnumber:
         print(f"{guess} is too high")
-        attempt += 1
-    else:
-        print(f"Congratulations you found the number in {attempt} attempts ! ")
+print(f"Congratulations you found the number in {attempt} attempts ! ")
